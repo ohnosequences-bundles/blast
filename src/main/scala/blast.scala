bad46d6b2da42086fbe24aa04c15493086dd8e1b
Marina Manrique <mmanrique@era7.com>
2015-05-02 17:05:27 +0200

    add code for blast bundle

--- src/main/scala/blast.scala
+++ src/main/scala/blast.scala
@@ -5,12 +5,32 @@ import ohnosequences.statika._, bundles._, instructions._
 
 case object blast {
 
+	case object ami extends amzn_ami_pv_64bit(Ireland)(1)
+
   case object Blast extends Bundle() {
 
     def install: Results = {
-      // do someting here
-      success(fullName + " is installed")
+
+      import ammonite.ops._
+      val wd = cwd
+
+      val getFiles =
+        Seq("aws", "s3", "cp", "s3://resources.ohnosequences.com/blast/ncbi-blast-2.2.30+-x64-linux.tar.gz", "./") -&- Seq("tar","-xvf", "ncbi-blast-2.2.30+-x64-linux.tar.gz")
+
+      val makeblastdbBin = "makeblastdbBin" 
+      val blastBinPath = "SPAdes-3.1.0-Linux"/"bin"/"*"
+      val usrbin = root/"usr"/"bin"
+
+      ln.s(wd/blastBinPath, usrbin/)
+
+      //Seq("ln", "-s","./SPAdes-3.1.0-Linux/bin/spades.py","/usr/bin/")
+
+      if ( exists(usrbin/makeblastdbBin) )
+        success(fullName + " is installed")
+      else
+        failure("Something went wrong with the linking :(")
     }
+
   }
 
 }
