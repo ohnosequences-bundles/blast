797239ac6bd956a8b38ac3d8056fe7cc86afc3d1
eparejatobes <eparejatobes@ohnosequences.com>
2015-08-05 18:17:12 +0200

    add record-based output format specs -- WIP

--- src/main/scala/blast.scala
+++ src/main/scala/blast.scala
@@ -77,7 +77,7 @@ object blastAPI {
   case class db(val file: File)             extends BlastOption( Seq("-db", file.getCanonicalPath().toString) )
   case class query(val file: File)          extends BlastOption( Seq("-query", file.getCanonicalPath().toString) )
   case class out(val file: File)            extends BlastOption( Seq("-out", file.getCanonicalPath().toString) )
-  case class evalue(val number: Long)       extends BlastOption( Seq("-evalue", number.toString) )
+  case class evalue(val number: Double)     extends BlastOption( Seq("-evalue", number.toString) )
   case class maxTargetSeqs(val number: Int) extends BlastOption( Seq("-max_target_seqs", s"${number}") )
   case object showGis                       extends BlastOption( Seq("-show_gis") )
   sealed trait BlastnTask extends AnyBlastOption
@@ -106,10 +106,126 @@ object blastAPI {
   case object JSONSeqalignOutputFormat              extends OutputFormat(12, outfmtSeq(12))
 
   // TODO fields for TSV and CSV
+  trait AnyOutputField {
+
+    lazy val blastRep: String = this.toString
+  }
+
+  import ohnosequences.cosas.properties._
+  import ohnosequences.cosas.typeSets._
+
+  trait AnyOutputRecordFormat {
+
+    type Fields <: AnyTypeSet.Of[out.AnyOutputField]
+    val fields: Fields
+  }
+
+  case class OutputRecordFormat[Flds <: AnyTypeSet.Of[out.AnyOutputField]]( val fields: Flds) extends AnyOutputRecordFormat {
+
+    type Fields = Flds
+  }
+
+  object out {
+
+    trait AnyOutputField extends AnyProperty
+    trait OutputField[V] extends AnyOutputField { type Raw = V }
+
+    // means Query Seq-id
+    case object qseqid    extends OutputField[String]         { val label = toString }
+    // means Query GI
+    case object qgi       extends OutputField[String]         { val label = toString }
+    // means Query accesion
+    case object qacc      extends OutputField[String]         { val label = toString }
+    // means Query accesion.version
+    case object qaccver   extends OutputField[Int]            { val label = toString }
+    // means Query sequence length
+    case object qlen      extends OutputField[Int]            { val label = toString }
+    // means Subject Seq-id
+    case object sseqid    extends OutputField[String]         { val label = toString }
+    // means All subject Seq-id(s), separated by a ';'
+    case object sallseqid extends OutputField[List[String]]   { val label = toString }
+    // means Subject GI
+    case object sgi       extends OutputField[String]         { val label = toString }
+    // means All subject GIs
+    case object sallgi    extends OutputField[List[String]]   { val label = toString }
+    // means Subject accession
+    case object sacc      extends OutputField[String]         { val label = toString }
+    // means Subject accession.version
+    case object saccver   extends OutputField[String]         { val label = toString }
+    // means All subject accessions
+    case object sallacc   extends OutputField[String]         { val label = toString }
+    // means Subject sequence length
+    case object slen      extends OutputField[Int]            { val label = toString }
+    // means Start of alignment in query
+    case object qstart    extends OutputField[Int]            { val label = toString }
+    // means End of alignment in query
+    case object qend      extends OutputField[Int]            { val label = toString }
+    // means Start of alignment in subject
+    case object sstart    extends OutputField[Int]            { val label = toString }
+    // means End of alignment in subject
+    case object send      extends OutputField[Int]            { val label = toString }
+    // means Aligned part of query sequence
+    case object qseq      extends OutputField[String]         { val label = toString }
+    // means Aligned part of subject sequence
+    case object sseq      extends OutputField[String]         { val label = toString }
+    // means Expect value
+    case object evalue    extends OutputField[Double]         { val label = toString }
+    // means Bit score
+    case object bitscore  extends OutputField[Long]           { val label = toString }
+    // means Raw score
+    case object score     extends OutputField[Long]           { val label = toString }
+    // means Alignment length
+    case object length    extends OutputField[Int]            { val label = toString }
+    // means Percentage of identical matches
+    case object pident    extends OutputField[Double]         { val label = toString }
+    // case object nident extends OutputField[String]  { // means Number of identical matches
+    // }
+    // means Number of mismatches
+    case object mismatch  extends OutputField[Int]      { val label = toString }
+    // means Number of positive-scoring matches
+    case object positive  extends OutputField[Int]      { val label = toString }
+    // means Number of gap openings
+    case object gapopen   extends OutputField[Int]      { val label = toString }
+    // means Total number of gaps
+    case object gaps      extends OutputField[Int]      { val label = toString }
+    // case object ppos extends OutputField[String]  { // means Percentage of positive-scoring matches
+    // }
+    // case object frames extends OutputField[String]  { // means Query and subject frames separated by a '/'
+    // }
+    // means Query frame
+    case object qframe      extends OutputField[String]   { val label = toString }
+    // means Subject frame
+    case object sframe      extends OutputField[String]   { val label = toString }
+    // case object btop extends OutputField[String]  { // means Blast traceback operations (BTOP)
+    // }
+    // case object staxids extends OutputField[String]  { // means unique Subject Taxonomy ID(s), separated by a ';' (in numerical order)
+    // }
+    // case object sscinames extends OutputField[String]  { // means unique Subject Scientific Name(s), separated by a ';'
+    // }
+    // case object scomnames extends OutputField[String]  { // means unique Subject Common Name(s), separated by a ';'
+    // }
+    // case object sblastnames extends OutputField[String]  { // means unique Subject Blast Name(s), separated by a ';' (in alphabetical order)
+    // }
+    // case object sskingdoms extends OutputField[String]  { // means unique Subject Super Kingdom(s), separated by a ';' (in alphabetical order)
+    // }
+    // case object stitle extends OutputField[String]  { // means Subject Title
+    // }
+    // case object salltitles extends OutputField[String]  { // means All Subject Title(s), separated by a '<>'
+    // }
+    // case object sstrand extends OutputField[String]  { // means Subject Strand
+    // }
+    // case object qcovs extends OutputField[String]  { // means Query Coverage Per Subject
+    // }
+    // case object qcovhsp extends OutputField[String]  { // means Query Coverage Per HSP
+    // }
+  }
+
+  import out._
+  val defaultOutputFormat = OutputRecordFormat(qseqid :~: sseqid :~: pident :~: length :~: mismatch :~: gapopen :~: qstart :~: qend :~: sstart :~: send :~: out.evalue :~: bitscore :~: ∅)
 
   case class blastCmd(val cmd: AnyBlastCommand, val opts: List[AnyBlastOption]) {
 
-    def toSeq: Seq[String] = Seq(cmd.name) ++ (opts flatMap { _.toSeq} ) 
+    def toSeq: Seq[String] = Seq(cmd.name) ++ (opts flatMap { _.toSeq } )
   }
 
   // TODO: something like this with HList-like cmd type etc
